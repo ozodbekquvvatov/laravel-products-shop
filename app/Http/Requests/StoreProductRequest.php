76b314e7a3b71f8eddc2e26a170cfdb7c17d6c19
Ozodbek Quvvatov <ozodbekquvvatov22@gmail.com>
2025-03-16 19:47:27 +0500

    added request for validate the form that for using in create

--- app/Http/Requests/StoreProductRequest.php
+++ app/Http/Requests/StoreProductRequest.php
@@ -21,6 +21,7 @@ class StoreProductRequest extends FormRequest
      */
     public function rules(): array
     {
+        
         return [
             'name' => 'required|string|max:255',
             'price' => 'required|numeric|min:0',
